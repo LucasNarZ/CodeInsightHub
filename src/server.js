dcebc4b05a93a8c08281c4e83e68cba6eab1e42a
LucasNarZ <LucasNarloch123@gmail.com>
2024-05-24 18:02:37 -0300

    initial GET by id feature

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.post('/pessoas',async (req, res) => {
                 req.body.stack = [];
             }
             //check if birchday is in correct format
-            if(key == "nascimento" && !(birthRegex).test(req.body[key])){
+            if(key == "nascimento" && !(birthRegex).test(req.body[key]) && typeof req.body[key] == "string"){
                 throw {name:"PrismaValidationError"}
             }
         })
@@ -48,20 +48,41 @@ app.post('/pessoas',async (req, res) => {
         res.setHeader('location', '/pessoas/' + userId);
         res.json(result);
     }catch(err){
-        
         if(err.name == "PrismaNullError"){
+            //for null parameter
             res.status(422);
             res.json(err.name);
         }else if(err.name == "PrismaClientKnownRequestError"){
+            //for unique constraint
             res.status(422);
             res.json(err.name);
-        }else{
+        }else if(err.name == "PrismaClientValidationError"){
+            //for wrong type parameter
             res.status(400);
+            res.json(err);
+        }else{
+            //for other errors
+            res.status(404);
             res.json(err);
         }
     }
 })
 
+app.get("/pessoas/:id", async (req, res) => {
+    try{
+        const user = await prisma.user.findUnique({
+            where:{
+                id:req.params.id
+            }
+        })
+        res.status(200);
+        res.json(user);
+    }catch(err){
+        console.log(err);
+    }
+    
+
+})
 
 
 let server = app.listen(port, () => {
