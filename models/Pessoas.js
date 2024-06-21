94666dbd90ee5355144453dd6a16ce2342ace048
LucasNarZ <LucasNarloch123@gmail.com>
2024-06-21 18:04:52 -0300

    adding pessoas model

---
+++ models/Pessoas.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const database = require("../utils/db");
+
+const Pessoas = database.define('pessoas', {
+    id : {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    apelido: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    stack: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: []
+    },
+    searchVector: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+        beforeCreate: (user, options) => {
+            user.searchVector = `${user.apelido} ${user.nome} ${user.stack.join(" ")}`;
+        },
+        beforeUpdate: (user, options) => {
+            user.searchVector = `${user.apelido} ${user.nome} ${user.stack.join(" ")}`;
+        }
+    }
+})
+
+module.exports = Pessoas;
