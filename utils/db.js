8993caa14b5641f6568fc97749758b71abc1415d
LucasNarZ <LucasNarloch123@gmail.com>
2024-06-21 18:02:30 -0300

    adding initial sequelize db config

---
+++ utils/db.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mydb', 'lucas', 'postgres', {
+    dialect: "postgres",
+    host: "localhost",
+    port: 5432
+});
+
+
+
+module.exports = sequelize;
