47464df7d0c1d52b17863c10bcb64b87c47bb282
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-01 22:21:53 -0300

    deleting unecessary files

--- db-test.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from '@sequelize/core';
-import { SqliteDialect } from '@sequelize/sqlite3';
-
-const sequelize = new Sequelize({
-  dialect: SqliteDialect,
-  storage: './test.db',
-});
-
-module.exports = sequelize;
