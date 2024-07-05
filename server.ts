ed171d7670ce43f755a179ab2d76b3bc9c9e9a2c
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-04 22:20:54 -0300

    getting back to cluster

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-import 'module-alias-jest/register';
+import 'module-alias/register';
 import express from "express";
 const app = express();
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 app.use(express.json());
 
 const port = process.env.HTTP_PORT ?? 4000;
@@ -15,25 +15,23 @@ app.use('/api/debug', debugRoutes);
 
 
 let server:any;
-// if(port != 4000){
-//     if(cluster.isMaster){
-//         for(let i = 0;i < numCPUs; i++){
-//             cluster.fork();
-//         }
-//     }else{
-//         server = app.listen(port, () => {
-//             console.log("Server is running at port", port);
-//         });
-//     }
-// }else{
-//     server = app.listen(port, () => {
-//         console.log("Server is running at port", port);
-//     });
-// }
-server = app.listen(port, () => {
-    console.log("Server is running at port", port);
-});
-
-
+if(port != 4000){
+    if(cluster.isMaster){
+        for(let i = 0;i < numCPUs; i++){
+            cluster.fork();
+        }
+    }else{
+        server = app.listen(port, () => {
+            console.log("Server is running at port", port);
+        });
+    }
+}else{
+    server = app.listen(port, () => {
+        console.log("Server is running at port", port);
+    });
+}
+// server = app.listen(port, () => {
+//     console.log("Server is running at port", port);
+// });
 
 export {server, app};
