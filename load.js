14be1c63d385ef8063bfe1b73c2405eb0b089155
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-11 19:53:43 -0300

    adding wrongBirth error and adjusting load test

--- load.js
+++ load.js
@@ -1,105 +1,64 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import { randomString, randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { check, sleep } from "k6";
 import { SharedArray } from 'k6/data';
-import { Rate } from 'k6/metrics';
 
-const failureRate = new Rate('failed_requests');
-
-const pessoasPayloads = new SharedArray('pessoasPayloads', function() {
-    return JSON.parse(open('./pessoas-payloads.json'));
+// Criando um array de 50000 usuários
+const users = new SharedArray("Users", function() {
+    let users = [];
+    for (let i = 0; i < 150000; i++) {
+        users[i] = { apelido: randomString(8), nome: "asdad", nascimento: "0000-00-00", stack: [] };
+    }
+    return users;
 });
 
-const termosBusca = new SharedArray('termosBusca', function() {
-    return (JSON.parse(open('./termos-busca.json'))).map(termo => {return termo.t});
-});
+const terms = new SharedArray("terms", function() {
+    let terms = [];
+    for (let i = 0; i < 150000; i++) {
+        terms[i] = randomString(8);
+    }
+    return terms;
+})
 
-const baseURL = 'http://localhost:9999';
-const userAgent = 'Agente do Caos - 2023';
-
-export let options = {
+const options = {
     scenarios: {
         criacaoEConsultaPessoas: {
             executor: 'ramping-arrival-rate',
-            startRate: 2,
-            timeUnit: '1s',
-            preAllocatedVUs: 10,
-            maxVUs: 600,
+            preAllocatedVUs: 1000,
             stages: [
-                { duration: '10s', target: 2 }, // warm up
-                { duration: '15s', target: 5 }, // ready?
-                { duration: '3m', target: 600 }, // lezzz go!!!
+                { duration: '10s', target: 20 }, // warm up
+                { duration: '15s', target: 50 }, // ready?
+                { duration: '1m', target: 600 }, // lezzz go!!!
             ],
         },
         buscaPessoas: {
             executor: 'ramping-arrival-rate',
-            startRate: 2,
-            timeUnit: '1s',
-            preAllocatedVUs: 10,
-            maxVUs: 100,
+            preAllocatedVUs: 100,
             stages: [
                 { duration: '25s', target: 2 }, // warm up
-                { duration: '3m', target: 100 }, // lezzz go!!!
+                { duration: '1m', target: 100 }, // lezzz go!!!
             ],
-        },
-        buscaInvalidaPessoas: {
-            executor: 'ramping-arrival-rate',
-            startRate: 2,
-            timeUnit: '1s',
-            preAllocatedVUs: 10,
-            maxVUs: 40,
-            stages: [
-                { duration: '25s', target: 2 }, // warm up
-                { duration: '3m', target: 40 }, // lezzz go!!!
-            ],
-        },
-    },
+        }
+    }
 };
+export { options };
 
-export default function() {
-    const payload = JSON.stringify(pessoasPayloads[Math.floor(Math.random() * pessoasPayloads.length)]);
-    const termosBuscaValue = termosBusca[Math.floor(Math.random() * termosBusca.length)];
-    let res;
-    // Criação e consulta de pessoas
-    // let res = http.post(`${baseURL}/api/pessoas`, payload, {
-    //     headers: { 'Content-Type': 'application/json'},
-    // });
-    // console.log(res);
-    // check(res, {
-    //     'status is 201, 422, or 400': (r) => [201, 422, 400].includes(r.status),
-    // });
+export default function () {
+    let user = users[randomIntBetween(0, 150000)];
+    let term = terms[randomIntBetween(0, 150000)];
 
-    // failureRate.add(res.status !== 201 && res.status !== 422 && res.status !== 400);
-
-    // if (res.status === 201) {
-    //     const location = res.headers['Location'];
-    //     if (location) {
-    //         res = http.get(`${baseURL}${location}`, {
-    //             headers: { 'User-Agent': userAgent },
-    //         });
-    //     }
-    // }
-
-    // // Busca válida de pessoas
-    // // res = http.get(`${baseURL}/api/pessoas?t=${termosBuscaValue.t}`, {
-    // //     headers: { 'User-Agent': userAgent },
-    // // });
-
-    // check(res, {
-    //     'status is 2xx': (r) => r.status >= 200 && r.status < 300,
-    // });
-
-    // failureRate.add(res.status < 200 || res.status >= 300);
-
-    // Busca inválida de pessoas
-    res = http.get(`${baseURL}/api/pessoas`, {
-        headers: { 'User-Agent': userAgent },
+    let res = http.post("http://localhost:9999/api/pessoas", JSON.stringify(user), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
     });
-    console.log(res);
     check(res, {
-        'status is 400': (r) => r.status === 400,
+        "status is 201 or 422": (r) => [201, 422].includes(r.status)
     });
 
-    failureRate.add(res.status !== 400);
-
-    sleep(Math.random() * (0.03 - 0.001) + 0.001);
-}
+    res = http.get("http://localhost:9999/api/pessoas?t=" + term);
+    check(res, {
+        "status is 200": (r) => r.status === 200
+    });
+    sleep(randomIntBetween(1, 10) / 100)
+};
--- src/controllers/users/findByTerm.ts
+++ src/controllers/users/findByTerm.ts
@@ -2,13 +2,12 @@ import { findByTermService } from "@services/users";
 
 export default async (req:ExpressRequest, res:ExpressResponse) => {
     try{
-        const searchedString:string = req.query.t as string;
-        
-        if(searchedString === NaN){
+        let searchedString:string = req.params.t;    
+        if(searchedString == ""){
             throw {name:"BadRequest"}
         }
         const users = await findByTermService(searchedString);
-        res.status(200).json(searchedString);
+        res.status(200).json(users);
     }catch(err){
         console.log(err);
         res.status(400);
