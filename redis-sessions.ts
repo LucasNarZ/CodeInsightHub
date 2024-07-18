6d3e21859575a2a3e05b55561e29cbe642905cac
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-17 22:10:18 -0300

    changing redis host

--- redis-sessions.ts
+++ redis-sessions.ts
@@ -1,11 +1,8 @@
 import Redis from "ioredis";
 
-const redisSessionsClient = new Redis();
-
-
-
-
-
-
+const redisSessionsClient = new Redis({
+    port:6379,
+    host:"172.21.0.2"
+});
 
 export default redisSessionsClient;
