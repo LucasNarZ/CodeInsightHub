04e865068af8d070cc9aa18d05e41193c016242b
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-20 10:45:58 -0300

    adding more functionalities to list input

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -11,6 +11,7 @@
             <Input name="Nome" placeholder="Lucas" /> 
             <Input name="Data de Nascimento" placeholder="2003-12-12" /> 
             <List name="Stack" placeholder="Python" />
+            <input type="submit" value="Submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mb-15"/>
         </div>
         
     </form>
--- Front-End/src/components/List.vue
+++ Front-End/src/components/List.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
-    import { ref, mounted, onMounted } from "vue"
+    import { ref } from "vue"
     import ListElement from './ListElement.vue'; 
     defineProps({
         name: String,
         placeholder: String
     })
     
-    const list = ref(["asdasd", "asdads"]);
-    const inputValue = ref("");
-    const input = ref(null);
-    let inputWidth = 0;
-    onMounted(() => {
-        inputWidth = input.offsetWidth
-    })
+    const list = ref<string[]>([]);
+    const inputValue = ref<string>("");
+    const input = ref<HTMLInputElement | null>(null);
+
+    function addToList(){
+        list.value.push(inputValue.value);
+        inputValue.value = "";
+        
+    }
+    function deleteElement(element:string){
+        console.log(element);
+    }
 </script>
 
 <template>
     <div class="w-full">
         <label :for="name" class="mb-2 text-lg-100 font-medium text-black">{{ name }}</label>
         <div class="relative">
-            <input type="text" :id="name" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" :placeholder="placeholder" required v-model="input" ref="input"/>
-            <button class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2" @click="list.push(inputValue)">Add</button>
+            <input type="text" :id="name" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" :placeholder="placeholder" required v-model="inputValue" ref="input"/>
+            <button type="button" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2" @click="addToList">Add</button>
         </div>
-        <div class="flex">
+        <div class="flex flex-wrap" @delete-element="deleteElement">
             <ListElement v-for="(item, index) in list" :key="index" :name="item"/>
         </div>
         
