799af410cea70fedb597fb8e2b4a4f66c5308341
LucasNarZ <LucasNarloch123@gmail.com>
2024-07-21 18:01:17 -0300

    submit logic

--- Front-End/src/App.vue
+++ Front-End/src/App.vue
@@ -1,20 +1,70 @@
-<script setup>
+<script setup lang="ts">
+    //@ts-ignore
     import Input from "./components/Input.vue";
     import List from "./components/List.vue";
+    import { FormKit } from "@formkit/vue";
+    interface Credentials {
+        apelido: String;
+        nome: String;
+        nascimento: String;
+        stack?: String[];
+    }
+    let list:string[] = [];
+    const inputClass =
+    "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-[400px]";
+    const labelClass = "block mb-2 text-lg-100 font-medium text-black";
+
+    function register(credentials: Credentials) {
+		credentials.stack = list;
+        console.log(credentials);
+    }
+    function getList(newList:string[]){
+		list = newList;
+    }
+
 </script>
 
 <template>
-    <form class="flex items-center flex-col">
-        <div class="max-w-[400px] w-full flex flex-col items-center space-y-10">
-            <h1 class="text-4xl mt-10">Dev Survey</h1>
-            <Input name="Apelido" placeholder="Lusqueta" /> 
-            <Input name="Nome" placeholder="Lucas" /> 
-            <Input name="Data de Nascimento" placeholder="2003-12-12" /> 
-            <List name="Stack" placeholder="Python" />
-            <input type="submit" value="Submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mb-15"/>
-        </div>
-        
-    </form>
-       
-
+  <FormKit
+    type="form"
+    form-class="flex items-center flex-col"
+    @submit="register"
+    :submit-attrs="{
+      label: 'Submit',
+      inputClass:
+        'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mb-15 mt-10',
+    }"
+  >
+    <div class="max-w-[400px] w-full flex flex-col items-center space-y-10">
+      <h1 class="text-4xl mt-10">Dev Survey</h1>
+      <FormKit
+        name="apelido"
+        type="text"
+        id="apelido"
+        :input-class="inputClass"
+        placeholder="Lusqueta"
+        label="apelido"
+        :label-class="labelClass"
+      />
+      <FormKit
+        name="nome"
+        type="text"
+        id="nome"
+        :input-class="inputClass"
+        placeholder="Lucas"
+        label="nome"
+        :label-class="labelClass"
+      />
+      <FormKit
+        name="nascimento"
+        type="text"
+        id="nascimento"
+        :input-class="inputClass"
+        placeholder="2006-12-12"
+        label="nascimento"
+        :label-class="labelClass"
+      />
+      <List label="Stack" placeholder="Python" name="stack" @get-list="getList"/>
+    </div>
+  </FormKit>
 </template>
